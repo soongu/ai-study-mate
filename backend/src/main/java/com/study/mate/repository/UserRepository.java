1c4e802490c271101025cf0ba09c6fee7e726600
soongu <naong526@naver.com>
2025-09-08 20:42:00 +0900

    사용자 리포지토리 추가: UserRepository 인터페이스를 생성하여 사용자 정보를 데이터베이스에서 관리할 수 있도록 함.

---
+++ backend/src/main/java/com/study/mate/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.study.mate.repository;
+
+import com.study.mate.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+    Optional<User> findByProviderId(String providerId);
+}
+
+
